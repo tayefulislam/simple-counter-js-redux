09c2dea87549562b5b01c60365f6c92f05adef2c
tayefulislam <kazitayefulislam12@gmail.com>
2022-08-25 23:16:31 +0600

    practice code part 1 done

--- index.js
+++ index.js
@@ -1,22 +1,85 @@
+// const counterEl = document.getElementById("counter");
+// const incrementEl = document.getElementById("increment");
+// const decrementEl = document.getElementById("decrement");
+
+
+// // initial state (step -1)
+
+// const initialState = {
+//     value: 0,
+// }
+
+// // create reducer Fuction  (step -2)
+
+// function counterReducer(state = initialState, action) {
+//     if (action.type === 'increment') {
+//         return {
+//             ...state,
+//             value: state.value + 1,
+//         }
+//     }
+//     else if (action.type === 'decrement') {
+//         return {
+//             ...state,
+//             value: state.value - 1,
+//         }
+//     }
+
+//     else {
+//         return state
+//     }
+// }
+
+// // create store (step -5)
+// const store = Redux.createStore(counterReducer)
+
+
+
+// // render to update ui (step -7)
+// const render = () => {
+//     const state = store.getState();
+//     counterEl.innerText = state.value.toString();
+// }
+// // render initail udpate the ui (step -8)
+// render()
+
+// // subscribe (step -6)
+// store.subscribe(render)
+
+// // onclick dispatch (step -4)
+
+// incrementEl.addEventListener("click", () => {
+
+//     store.dispatch({
+//         type: "increment",
+//     });
+// });
+// decrementEl.addEventListener("click", () => {
+
+//     store.dispatch({
+//         type: "decrement",
+//     });
+// });
+
+
+
+const initialState = {
+    value: 0
+}
 const counterEl = document.getElementById("counter");
 const incrementEl = document.getElementById("increment");
 const decrementEl = document.getElementById("decrement");
 
+// create reducer fucntion
 
-// initial state
+function createReducer(state = initialState, action) {
 
-const initialState = {
-    value: 0,
-}
-
-// create reducer Fuction 
-
-function counterReducer(state = initialState, action) {
     if (action.type === 'increment') {
         return {
             ...state,
             value: state.value + 1,
         }
+
     }
     else if (action.type === 'decrement') {
         return {
@@ -28,24 +91,37 @@ function counterReducer(state = initialState, action) {
     else {
         return state
     }
+
+}
+// create redux store 
+
+
+const store = Redux.createStore(createReducer)
+
+const render = () => {
+    const state = store.getState();
+    counterEl.innerText = state.value.toString();
 }
 
-// create store
-const store = Redux.createStore(counterReducer)
+render()
+
+store.subscribe(render);
 
 
-// onclick dispatch
+
+// onclick dispactch
 
 
 incrementEl.addEventListener("click", () => {
-
     store.dispatch({
         type: "increment",
-    });
-});
-decrementEl.addEventListener("click", () => {
+    })
 
-    store.dispatch({
-        type: "decrement",
-    });
 });
+
+decrementEl.addEventListener("click", () => {
+    store.dispatch({ type: "decrement" })
+});
+
+
+
