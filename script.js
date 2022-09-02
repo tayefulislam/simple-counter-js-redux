37ace69cf4cd13edc9961e4fa5905d9836d57dac
tayefulislam <kazitayefulislam12@gmail.com>
2022-09-02 10:14:09 +0600

    action payload and cretors created

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
-
-
-const counterEl = document.getElementById("counter");
-const incrementEl = document.getElementById("increment");
-const decrementEl = document.getElementById("decrement");
 const initialState = {
     value: 0,
 }
 
+const counterEl = document.getElementById("counter");
+const incrementEl = document.getElementById("increment");
+const decrementEl = document.getElementById("decrement");
+
+
+// action indentifiers
+
+const INCREMENT = 'increment';
+const DECREMENT = 'decrement';
+
+
+
+// action creators
+const increment = (value) => {
+    return {
+        type: INCREMENT,
+        payload: value,
+    }
+}
+
+const decrement = (value) => {
+    return {
+        type: DECREMENT,
+        payload: value,
+    }
+}
+
+
+
 // create reducer funtion
 function createReducer(state = initialState, action) {
-    if (action.type === 'increment') {
+    console.log(action)
+    if (action.type === INCREMENT) {
 
         return {
             ...state,
-            value: state.value + 1,
+            value: state.value + action.payload,
         }
 
     }
 
-    else if (action.type === 'decrement') {
+    else if (action.type === DECREMENT) {
         return {
             ...state,
-            value: state.value - 1,
+            value: state.value - action.payload,
         }
     }
     else { return state }
@@ -47,15 +72,11 @@ store.subscribe(render)
 
 
 incrementEl.addEventListener("click", () => {
-    store.dispatch({
-        type: "increment"
-    });
+    store.dispatch(increment(3));
 });
 
 decrementEl.addEventListener("click", () => {
-    store.dispatch({
-        type: 'decrement'
-    });
+    store.dispatch(decrement(1));
 });
 
 
